3abe92caee289ee658c2dee9adf7a074c262bef5
Sudeep <sudeepn15@gmail.com>
2023-10-17 23:55:32 +0530

    add loader

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
+
 const accordian = document.querySelectorAll(".accordian");
 const submitBtn = document.querySelector("button[type=submit]");
 const form = document.querySelector("form");
