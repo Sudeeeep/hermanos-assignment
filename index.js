2ee6c2c7be269c356c98d69cd619759aadeb4c76
Sudeep <sudeepn15@gmail.com>
2023-10-17 16:45:26 +0530

    add details section

---
+++ index.js
@@ -0,0 +1,14 @@
+const accordian = document.querySelectorAll(".accordian");
+const submitBtn = document.querySelector("button[type=submit]");
+const form = document.querySelector("form");
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  form.reset();
+});
+
+for (let i = 0; i < accordian.length; i++) {
+  accordian[i].addEventListener("click", (e) => {
+    e.currentTarget.classList.toggle("active"); // add class of active to accordian
+  });
+}
