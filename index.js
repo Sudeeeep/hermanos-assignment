aff93f5cff8d98ede1951b9dbe2a0e70fbf0ee00
Sudeep <sudeepn15@gmail.com>
2023-10-17 23:36:56 +0530

    add responsive

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-new Glide(".glide").mount();
-
 const accordian = document.querySelectorAll(".accordian");
 const submitBtn = document.querySelector("button[type=submit]");
 const form = document.querySelector("form");
+const headerMenu = document.querySelector(".header-menu");
+const mobileHeaderDetails = document.querySelector(".mobile-header-details");
+const primaryNavMenu = document.querySelector(".primary-nav-bar-menu");
+const mobileNavBar = document.querySelector(".primary-mobile-nav-bar-list");
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -14,3 +16,19 @@ for (let i = 0; i < accordian.length; i++) {
     e.currentTarget.classList.toggle("active"); // add class of active to accordian
   });
 }
+
+headerMenu.addEventListener("click", (e) => {
+  if (mobileHeaderDetails.style.display === "none") {
+    mobileHeaderDetails.style.display = "flex";
+  } else {
+    mobileHeaderDetails.style.display = "none";
+  }
+});
+
+primaryNavMenu.addEventListener("click", (e) => {
+  if (mobileNavBar.style.display === "none") {
+    mobileNavBar.style.display = "flex";
+  } else {
+    mobileNavBar.style.display = "none";
+  }
+});
